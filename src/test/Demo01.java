ef1dda019ce9e1a19ba74a19795fe69ec9c111de
nnn <1741847900@qq.com>
2021-07-28 14:48:43 +0800

    test 01

---
+++ src/test/Demo01.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author nnn🐉
+ * @date 2021/7/28 14:40
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+    }
+}
